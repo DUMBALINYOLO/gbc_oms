d52a20f70a9f37b09f2f592089e9245d43cc45f1
DUMBALINYOLO <baridzimaximillem@gmail.com>
2021-02-17 10:02:13 +0200

    'gaggagaggaa'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import {HashRouter as Router} from 'react-router-dom';
 import store from './store';
 import {Provider} from 'react-redux';
 import 'sanitize.css/sanitize.css';
@@ -16,5 +16,3 @@ ReactDOM.render(
     </Provider>,
   document.getElementById('app')
 );
-
-
